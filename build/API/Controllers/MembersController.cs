0e21ff2fa76f5c4de84be55200d42239876e4d93
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-22 23:02:29 +0530

    Controller endpoints basics

--- build/API/Controllers/MembersController.cs
+++ build/API/Controllers/MembersController.cs
@@ -1,11 +1,51 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] //localhost:50001/api/members
     [ApiController]
-    public class MembersController : ControllerBase
+    public class MembersController(AppDbContext context) : ControllerBase
     {
+        //Synchronous Task
+        // [HttpGet]
+        // public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        // {
+        //     var members = context.Users.ToList();
+        //     return members;
+        // }
+
+        // [HttpGet("{id}")] //localhost:50001/api/members/bob-id
+        // public ActionResult<AppUser> GetMember(string id)
+        // {
+        //     var member = context.Users.Find(id);
+        //     if (member == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return member;
+        // }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembersAsync()
+        {
+            var members = await context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")] //localhost:50001/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMemberAsync(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return member;
+        }
+
     }
 }
