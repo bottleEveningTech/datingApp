106e368ec9e635405ccf9763335ae3156d1b9927
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-24 00:11:29 +0530

    Client added and Track client folder normally instead of submodule

---
+++ client/src/app/app.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+  protected readonly title = 'Dating App';
+  protected members: any;
+  /*
+    zone less change detection for angular 20
+   protected members = signal<any>([]); fine grained change detection
+  */
+
+
+
+  // Flow of component start: ctor -> component initialize using oninit interface
+  // constructor(private http: HttpClient) { //Angular DI system way 1
+  // }
+
+  /*
+   Angular DI system way 2 using inject()
+   */
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (response)=> {
+        this.members.set(response);
+      },
+      error: (error)=> {
+        console.log(error);
+      },
+      complete: ()=> {
+        console.log('completed request');
+      }
+    })
+  }
+
+  /*
+  using promises and signals 
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch(error){
+      console.log(error);
+      throw error;
+    }
+  */
+}
